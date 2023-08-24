3d7e1e7057f8188210ed41a3ec0ef056139fbf34
Mariano Labatut <marianolabatut@gmail.com>
2023-08-23 21:46:55 -0300

    Primeros endpoints

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Nuevo endpoint')
+})
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  })
+})
+
+app.listen(port, () => {
+  //
+})
